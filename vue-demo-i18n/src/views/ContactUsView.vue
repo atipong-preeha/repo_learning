1bb89215e0bb6ea200f15c347036beeca4b3f554
Atipong Preehajindasawat <atipong.pre@roojai.com>
2022-12-07 13:35:52 +0700

    add new

---
+++ vue-demo-i18n/src/views/ContactUsView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="contactUs">
+      <h1>{{ $t('contactUs.content') }}</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .contactUs {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
